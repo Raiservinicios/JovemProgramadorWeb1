d33d21f4980f973b591749dd1737840660a41d68
Raiservinicios <vinicios@osraiser.com.br>
2023-12-05 16:06:20 -0300

    Adicionado plano de fundo, recuperar senha

--- JovemProgramadorWeb1/Controllers/EsqueceuSenhaController.cs
+++ JovemProgramadorWeb1/Controllers/EsqueceuSenhaController.cs
@@ -4,9 +4,67 @@ namespace JovemProgramadorWeb1.Controllers
 {
     public class EsqueceuSenhaController : Controller
     {
+<<<<<<< Updated upstream
+=======
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+
+        public EsqueceuSenhaController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+        
+
+        [HttpGet]
+>>>>>>> Stashed changes
         public IActionResult EsqueceuSenhaIndex()
         {
             return View();
         }
+<<<<<<< Updated upstream
+=======
+
+        [HttpPost]
+
+        [HttpPost]
+        public IActionResult AlterarSenha(string nomeUsuario, string respostaSeguranca, string novaSenha)
+        {
+            Usuario usuario = new Usuario
+            {
+                nomeUsuario = nomeUsuario,
+                respostaSeguranca = respostaSeguranca,
+                senha = novaSenha
+            };
+
+            var usuarioValidado = _usuarioRepositorio.ValidarResposta(usuario);
+
+            if (usuarioValidado != null && VerificarRespostaSeguranca(usuarioValidado, respostaSeguranca))
+            {
+                usuarioValidado.senha = novaSenha;
+                _usuarioRepositorio.AtualizarSenha(usuarioValidado);
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        private bool VerificarRespostaSeguranca(Usuario usuario, string respostaSeguranca)
+        {
+
+            if (usuario != null && !string.IsNullOrEmpty(usuario.respostaSeguranca))
+            {
+
+                return string.Equals(usuario.respostaSeguranca, respostaSeguranca, StringComparison.OrdinalIgnoreCase);
+            }
+
+            return false;
+        }
+>>>>>>> Stashed changes
     }
 }
--- JovemProgramadorWeb1/Models/Usuario.cs
+++ JovemProgramadorWeb1/Models/Usuario.cs
@@ -1,11 +1,16 @@
-﻿namespace JovemProgramadorWeb1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JovemProgramadorWeb1.Models
 {
     public class Usuario
     {
+
         public int codigo { get; set; }
         public string nomeUsuario { get; set; }
         public string senha { get; set; }
         public string respostaSeguranca { get; set; }
         public bool flagStatus { get; set; }
+
+        public string perguntaSeguranca { get; set; }
     }
 }
